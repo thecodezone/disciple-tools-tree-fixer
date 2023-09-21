46b957fdd25d707797d48ddde91c8cdca8404c0a
Craig Wann <craig@codezone.io>
2023-09-21 13:57:43 -0500

    Adding tests.

--- test/unit-test-plugin.php
+++ test/unit-test-plugin.php
@@ -4,10 +4,10 @@ class PluginTest extends TestCase
 {
     public function test_plugin_installed()
     {
-        activate_plugin( 'disciple-tools-plugin-starter-template/disciple-tools-plugin-starter-template.php' );
+        activate_plugin( 'disciple-tools-tree-fixer/disciple-tools-tree-fixer.php' );
 
         $this->assertContains(
-            'disciple-tools-plugin-starter-template/disciple-tools-plugin-starter-template.php',
+	        'disciple-tools-tree-fixer/disciple-tools-tree-fixer.php',
             get_option( 'active_plugins' )
         );
     }
